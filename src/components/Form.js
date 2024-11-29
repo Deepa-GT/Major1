bf875d843c7242d21d43b91861da93b676361596
Deepa-GT <deepagt445@gmail.com>
2024-11-29 19:03:39 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,16 @@ import React from "react";
 function Form() {
   return (
     <div className="main">
-      <p className="sign" align="center">
+        <p className="sign" align="center">
         Sign in
-      </p>
-      <form className="form1">
-        <input className="username" type="text" placeholder="Username" />
-        <input className="password" type="password" placeholder="Password" />
-        <a className="submit" align="center">
-          Sign in
-        </a>
-       
         </p>
-      </form>
+        <form className="form1">
+            <input className="username" type="text" placeholder="Username" />
+             <input className="password" type="password" placeholder="Password" />
+             <a className="submit" align="center">
+             Sign in
+             </a>
+        </form>
     </div>
   );
 }
